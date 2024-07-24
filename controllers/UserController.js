9241efede7bf721b92a23e6319a0ecd8b4eecb5a
kevin <kevin.naaya@gmail.com>
2024-07-24 16:36:46 +0530

    fixes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,27 +4,30 @@ exports.register = async (req, res) => {
     try {
         const { name, userId, password } = req.body;
 
-        const checkUserIdExist = await User.findOne({ userId });
+        // Check if userId already exists
+        const checkUserIdExist = await User.findOne({ userId: userId.toLowerCase() });
 
         if (checkUserIdExist) {
             return res.status(400).send({
-                message: "User already exist. Try another unique User ID",
+                message: "User already exists. Try another unique User ID",
             });
         }
 
-        const newUser = {
+        // Create new user object
+        const newUser = new User({
             userId: userId.toLowerCase(),
             name,
             password,
-        };
+        });
 
-        await User.create(newUser);
+        // Save new user to database
+        await newUser.save();
 
-        return res.status(200).send({
+        return res.status(201).send({
             message: "New user created successfully"
         });
     } catch (err) {
-        res.status(500).send({
+        return res.status(500).send({
             message: err.message,
         });
     }
@@ -32,34 +35,43 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
     try {
-        let { userId, password } = req.body;
+        const { userId, password } = req.body;
 
-        userId = userId.toLowerCase();
+        // Find user by userId
+        const user = await User.findOne({ userId: userId.toLowerCase() }).select("+password");
 
-        const user = await User.findOne({ userId }).select("+password");
         if (!user) {
             return res.status(400).send({
                 message: "User does not exist",
             });
         }
+
+        // Check if password matches
         const isMatch = await user.matchPassword(password);
 
         if (!isMatch) {
             return res.status(400).send({
-                message: "incorrect password",
+                message: "Incorrect password",
             });
         }
 
-        user.password = "";
+        // Clear the password before sending the response
+        user.password = undefined;
 
+        // Generate token
         const token = await user.generateToken();
+        const options = {
+            expires: new Date(Date.now() + 360 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+        };
 
-        return res.status(200).cookie("token", token).send({
+        // Send token in a cookie
+        return res.status(200).cookie("token", token, options).send({
             user,
             token,
         });
     } catch (err) {
-            return res.status(500).send({
+        return res.status(500).send({
             message: err.message,
         });
     }
